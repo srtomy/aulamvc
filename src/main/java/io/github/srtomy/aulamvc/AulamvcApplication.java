7c24777b02a58fc8d4fb6635bacd8811a0bbf003
Tarcisio Scotta <tarcisio.scotta@assessorplan.com>
2020-09-18 16:36:06 -0300

    add classe main do Spring

---
+++ src/main/java/io/github/srtomy/aulamvc/AulamvcApplication.java
@@ -0,0 +1,13 @@
+package io.github.srtomy.aulamvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulamvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulamvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/srtomy/aulamvc/AulamvcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.srtomy.aulamvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulamvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
